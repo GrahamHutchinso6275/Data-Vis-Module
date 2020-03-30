4474987f5141aaccdb0ad49d1329208ec664d831
gg <grahamhutch123@gmail.com>
2020-03-30 02:37:51 +0100

    Loading csv data from file

--- choropleth.py
+++ choropleth.py
@@ -3,16 +3,25 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-shapefile = 'Brazil_Admin_1.shp'
-#shapefile = 'Brasil.shx'
-gdf = gpd.read_file(shapefile) #['ADMIN', 'ADM0_A3', 'geometry']
-print(gdf.head())
+#loads a shape file and plots it.
 
-gdf.plot()
-plt.show()
-
-
-#world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-#print(world.head())
-#world.plot()
+#shapefile = 'Brazil_Admin_1.shp'
+#gdf = gpd.read_file(shapefile)
+#print(gdf.head)
+#gdf.plot()
 #plt.show()
+
+#datafile = 'amazon.csv'
+#df = pd.read_csv(datafile, encoding='ISO-8859-1')
+
+#print(df.head())
+
+#shapefile = 'data/countries_110m/ne_110m_admin_0_countries.shp'#Read shapefile using Geopandas
+#gdf = gpd.read_file(shapefile)[['ADMIN', 'ADM0_A3', 'geometry']]#Rename columns.
+#gdf.columns = ['country', 'country_code', 'geometry']
+#gdf.head()
+
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+print(world.head())
+world.plot()
+plt.show()
