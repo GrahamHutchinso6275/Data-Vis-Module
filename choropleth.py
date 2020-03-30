aa707074a1ea45bd4a3fdce8ca0d2e5fedf6e34a
gg <grahamhutch123@gmail.com>
2020-03-30 01:43:28 +0100

    choropleth.py can now display shape files, first milestone crossed!

--- choropleth.py
+++ choropleth.py
@@ -3,14 +3,16 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-#shapefile = 'Brazil_Admin_1.shp'
+shapefile = 'Brazil_Admin_1.shp'
+#shapefile = 'Brasil.shx'
+gdf = gpd.read_file(shapefile) #['ADMIN', 'ADM0_A3', 'geometry']
+print(gdf.head())
 
-#gdf = gpd.readfile(shapefile)['ADMIN', 'ADM0_A3', 'geometry']
-
-#gdf.head()
-
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-print(world.head())
-
-world.plot()
+gdf.plot()
 plt.show()
+
+
+#world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+#print(world.head())
+#world.plot()
+#plt.show()
